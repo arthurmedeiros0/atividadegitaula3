20090b5413a9f737234f916eb341fcb12915e09a
Arthur <arthurdinizazevedo@gmail.com>
2026-05-21 16:50:33 -0300

    Primeira parte

---
+++ index.js
@@ -0,0 +1,17 @@
+function soma(a, b) {
+  return a + b;
+}
+
+function subtracao(a, b) {
+  return a - b;
+}
+
+function multiplicacao(a, b) {
+  return a * b;
+}
+function divisao(a, b) {
+    if (b === 0) {
+        return "Não é possível dividir por zero.";
+    }
+    return a / b;
+}
